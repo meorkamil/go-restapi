3390440ae6e6d4e98d542c061f77eea8cbdaebed
Algojo <36105680+meorkamil@users.noreply.github.com>
2024-10-11 21:26:42 +0800

    adding logger (#2)
    bump version

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,30 +1,26 @@
-package store
+package database
 
 import (
 	"database/sql"
 	"go-restapi/internal/model"
-	"log"
+	"go-restapi/internal/util"
 
 	_ "github.com/lib/pq"
 )
 
-var db *sql.DB
-
-//type Store interface {
-//	Select(emp *model.Employee)
-//	ListEmployee()
-//}
+var (
+	db *sql.DB
+	lg = util.NewLogger()
+)
 
 func InitDB(dsn string) error {
-
 	var err error
 
-	log.Println("Connecting to database:", dsn)
+	lg.Info("Connecting to database:", dsn)
 
 	db, err = sql.Open("postgres", dsn)
-
 	if err != nil {
-		log.Fatal(err)
+		lg.Fatal(err)
 
 	}
 
@@ -36,20 +32,17 @@ func Select(emp *model.Employee) []model.Employee {
 	rows, err := db.Query("SELECT * FROM employee WHERE empid = ($1) LIMIT 1", emp.Empid)
 
 	if err != nil {
-		log.Fatal("Select data failed ", err)
+		lg.Fatal("Select data failed ", err)
 		return nil
 	}
 
 	var listOfEmployee []model.Employee
 
 	for rows.Next() {
-
 		var emp model.Employee
-
 		if err := rows.Scan(&emp.Empid, &emp.Name, &emp.Dept); err != nil {
 
-			log.Fatal("Error mapping data")
-
+			lg.Fatal("Error mapping data")
 		}
 
 		listOfEmployee = append(listOfEmployee, emp)
@@ -59,11 +52,10 @@ func Select(emp *model.Employee) []model.Employee {
 }
 
 func ListEmployee() []model.Employee {
-
 	rows, err := db.Query("SELECT * FROM employee;")
 
 	if err != nil {
-		log.Fatal("Select data failed ", err)
+		lg.Fatal("Select data failed ", err)
 		return nil
 	}
 
@@ -72,10 +64,9 @@ func ListEmployee() []model.Employee {
 	for rows.Next() {
 
 		var emp model.Employee
-
 		if err := rows.Scan(&emp.Empid, &emp.Name, &emp.Dept); err != nil {
 
-			log.Fatal("Error mapping data")
+			lg.Fatal("Error mapping data")
 
 		}
 
@@ -83,5 +74,4 @@ func ListEmployee() []model.Employee {
 	}
 
 	return listOfEmployee
-
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,13 +1,16 @@
 package server
 
 import (
+	"go-restapi/internal/database"
 	"go-restapi/internal/model"
 	"go-restapi/internal/router"
-	"go-restapi/internal/store"
-	"log"
+	"go-restapi/internal/util"
+	"net"
 	"net/http"
 	"strconv"
 )
+
+var lg = util.NewLogger()
 
 type APIServer struct {
 	listenAddr string
@@ -15,32 +18,27 @@ type APIServer struct {
 }
 
 func NewAPIServer(cfg *model.Config) *APIServer {
-
-	listenAddr := cfg.Server.Addr + ":" + strconv.Itoa(cfg.Server.Port)
 	dbDsn := "postgresql://" + cfg.Database.User + ":" + cfg.Database.Pass + "@" + cfg.Database.Host + "/" + cfg.Database.DBName + cfg.Database.DBFlags
 
 	return &APIServer{
-		listenAddr: listenAddr,
+		listenAddr: net.JoinHostPort(cfg.Server.Addr, strconv.Itoa(cfg.Server.Port)),
 		dbDsn:      dbDsn,
 	}
-
 }
 
-func (s *APIServer) Run() {
-
+func (s *APIServer) Run() error {
 	mux := router.CreateRouter()
 
-	if err := store.InitDB(s.dbDsn); err != nil {
+	if err := database.InitDB(s.dbDsn); err != nil {
 
-		log.Fatal("Init DB failed !!!")
+		lg.Fatal("Init DB failed !!!", err)
 	}
 
-	log.Println("Starting server:", s.listenAddr)
+	lg.Info("Starting server:", s.listenAddr)
 
 	if err := http.ListenAndServe(s.listenAddr, mux); err != nil {
-
-		log.Fatal("Failed to start server", err)
-
+		lg.Fatal("Failed to start server", err)
 	}
 
+	return nil
 }
